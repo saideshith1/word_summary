efae2fc075228f855b769326024c421afacce275
saideshith1 <saiexams1905@gmail.com>
2024-12-28 17:06:28 +0000

    adding changes in tesT_wikibot

--- test_wikibot.py
+++ test_wikibot.py
@@ -4,8 +4,6 @@ from wikibot import search_cli
 def test_search():
     assert "Microsoft" in search("Microsoft",sentences=2)
 
-
-
 def test_wiki_bot():
   runner = CliRunner()
   result = runner.invoke(search_cli, ["--name","Microsoft","--sentences","2"])
