0e7c6090b5db304af21477588b0ed78db3f817db
saideshith1 <saiexams1905@gmail.com>
2024-12-28 17:09:09 +0000

    correctin errors

--- test_wikibot.py
+++ test_wikibot.py
@@ -1,11 +1,14 @@
 from mylib.bot import search
 from click.testing import CliRunner
 from wikibot import search_cli
+
+
 def test_search():
-    assert "Microsoft" in search("Microsoft",sentences=2)
+    assert "Microsoft" in search("Microsoft", sentences=2)
+
 
 def test_wiki_bot():
-  runner = CliRunner()
-  result = runner.invoke(search_cli, ["--name","Microsoft","--sentences","2"])
-  assert result.exit_code == 0
-  assert "Microsoft" in result.output
+    runner = CliRunner()
+    result = runner.invoke(search_cli, ["--name", "Microsoft", "--sentences", "2"])
+    assert result.exit_code == 0
+    assert "Microsoft" in result.output
--- wikibot.py
+++ wikibot.py
@@ -2,11 +2,12 @@ from mylib.bot import search
 import click
 
 @click.command()
-@click.option('--name', prompt='webpage to search', help='webpage we want to search')
-@click.option('--sentences', prompt='number sentences', help='number of sentences u want to print')
-def search_cli(name = "Microsoft", sentences = 1):
+@click.option("--name", prompt="webpage to search", help="webpage we want to search")
+@click.option("--sentences", prompt="number sentences", help="number of sentences u want to print")
+def search_cli(name="Microsoft", sentences=1):
     result = search(name, sentences=sentences)
     click.echo(result)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     search_cli()
